34205d8d9a9bc80c3179ac296bc894e5221784d3
samimOsman754 <msosman754@gmail.com>
2026-02-28 15:11:42 +0600

    See work button cutomize

--- components/Home/Hero/Hero.tsx
+++ components/Home/Hero/Hero.tsx
@@ -3,6 +3,7 @@ import Image from "next/image";
 import { BsArrowRight } from "react-icons/bs";
 import Typewriter from "typewriter-effect";
 import ParticlesHero from "./ParticleBackground";
+import Link from "next/link";
 
 const Hero = () => {
   return (
@@ -48,14 +49,16 @@ const Hero = () => {
             />
           </span>
         </h2>
-        <button
-          data-aos="fade-up"
-          data-aos-delay="600"
-          className="mt-6 px-10 py-4 bg-blue-800 hover:bg-blue-900 transition-all duration-300 cursor-pointer rounded-full text-lg font-medium"
-        >
-          <span>See my work</span>
-          <BsArrowRight className="w-5 h-5 ml-2 inline-block" />
-        </button>
+        <Link href="#projects">
+          <button
+            data-aos="fade-up"
+            data-aos-delay="600"
+            className="mt-6 px-10 py-4 bg-blue-800 hover:bg-blue-900 transition-all duration-300 cursor-pointer rounded-full text-lg font-medium"
+          >
+            <span>See my work</span>
+            <BsArrowRight className="w-5 h-5 ml-2 inline-block" />
+          </button>
+        </Link>
       </div>
     </div>
   );
--- components/Home/Navbar/Nav.tsx
+++ components/Home/Navbar/Nav.tsx
@@ -27,14 +27,17 @@ const Nav = ({ openNav }: Props) => {
     >
       <div className="flex items-center h-full justify-between w-[90%] mx-auto">
         {/* Logo */}
-        <div className="flex items-center space-x-2">
-          <div className="w-10 h-10 bg-white rounded-full flex items-center justify-center flex-col">
-            <FaCode className="w-5 h-5 text-black" />
+        <Link href="#home">
+          <div className="flex items-center space-x-2">
+            <div className="w-10 h-10 bg-white rounded-full flex items-center justify-center flex-col">
+              <FaCode className="w-5 h-5 text-black" />
+            </div>
+            <h1 className="text-xl hidden sm:block md:text-2xl text-white font-bold">
+              SAMIM
+            </h1>
           </div>
-          <h1 className="text-xl hidden sm:block md:text-2xl text-white font-bold">
-            SAMIM
-          </h1>
-        </div>
+        </Link>
+
         {/* Nav Links */}
         <div className="hidden lg:flex items-center space-x-10">
           {NavLinks.map((link) => {
